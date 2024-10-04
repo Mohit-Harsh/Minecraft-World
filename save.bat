b9ec774b24cd29c6d8700a1da60c89f1d98024d1
Mohit Harsh <mohitharshxiib17@gmail.com>
2024-10-04 07:10:10 +0530

    added save.sh and save.bat

---
+++ save.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m 'gameplay'
+git push origin main
--- start.bat
+++ start.bat
@@ -1,1 +1,4 @@
+git pull origin main
 java -Xmx2048M -Xms2048M -jar server.jar nogui
+git commit -m 'gameplay'
+git push origin main
